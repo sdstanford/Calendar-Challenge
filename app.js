fe91779937aff918c3ded445897431ae80b2a91d
Shannon Doepke <shannondoepke@Shannons-Air.lan>
2018-06-05 14:07:26 -0400

    Event constructor and modal input handling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var events = [];
 
 // New Event Object Constructor
-function Event(title, location, date, start, end) {
+function Event(title, location, date, allDay, start, end) {
     this.title = title,
     this.location = location,
     this.date = date,
@@ -9,8 +9,24 @@ function Event(title, location, date, start, end) {
     this.end = end
 };
 
+//Create new event based on inputs in modal
+var createNewEvent = function (event) {
+    event.preventDefault();
+
+    var title = $("#title").val().trim();
+    var location = $("#location").val().trim();
+    var date = $("#date").val().trim();
+    var allDay = $("#alldaycheck").val();
+    var start = $("#starttime").val();
+    var end = $("#endtime").val();
+
+    var newEvent = new Event(title, location, date, allDay, start, end);
+}
+
 $(document).ready(function(){
     $("td").dblclick(function(){
         $("#myModal").modal();
     });
 });
+
+$("#submit").on("click", createNewEvent());
