2b2f9c20f7c22ff675a57e4fefcf55aae70ba98e
Shannon Doepke <shannondoepke@Shannons-Air.lan>
2018-06-05 18:11:51 -0400

    Add individual event modal

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var events = [{title: "Running Club", location: "blah", date: "2018-06-06", start: "9", end: "10"}];
+var events = [{date:"2018-06-21", end:"10", id:31, location:"Downtown", start:"9", title:"Yoga"}];
+var x = 0;
 
 //Identify today's date with red circle
 // var today = new Date();
@@ -9,12 +10,13 @@ var events = [{title: "Running Club", location: "blah", date: "2018-06-06", star
 
 
 // New Event Object Constructor
-function Event(title, location, date, start, end) {
+function Event(title, location, date, start, end, id) {
     this.title = title,
     this.location = location,
     this.date = date,
     this.start = start,
-    this.end = end
+    this.end = end,
+    this.id = id
 };
 
 //Initiate modal pop up when calendar cell is double clicked
@@ -34,8 +36,9 @@ var addNewEvent = function() {
     var date = $("#date").val().trim();
     var start = $("#starttime").val();
     var end = $("#endtime").val();
+    var id = Math.floor((Math.random() * 100) + 1);
 
-    var newEvent = new Event(title, location, date, start, end);
+    var newEvent = new Event(title, location, date, start, end, id);
 
     events.push(newEvent);
 
@@ -45,6 +48,7 @@ var addNewEvent = function() {
 };
 
 var displayEvents = function() {
+
     events.forEach(function(event){
         //Split event date at dash to identify day
         var datesplit = event.date.split("-");
@@ -56,6 +60,14 @@ var displayEvents = function() {
         var eventName = $("<a>");
         eventName.text(event.title);
         eventName.addClass("event-link");
+        eventName.attr("id", "event" + event.id);
+        eventName.attr("data-toggle", "modal");
+        eventName.attr("data-target", "#eventModal");
+        eventName.attr("data-id", "event" + event.id);
+        eventName.attr("data-title", event.title);
+        eventName.attr("data-location", event.location);
+        eventName.attr("data-date", event.date);
+        eventName.attr("data-time", event.start + " - " + event.end);
         //Append link to event div
         eventDiv.append(eventName);
         //Append both to appropriate calendar field
@@ -63,7 +75,18 @@ var displayEvents = function() {
     })
 }
 
-$(".event-link").on("click", function(){
-    alert(event.title, event.location, event.start, event.end);
-})
+$('#eventModal').on('show.bs.modal', function (event) {
+    var a = $(event.relatedTarget) // Button that triggered the modal
+    var title = a.data('title') // Extract info from data-* attributes
+    var location = a.data('location')
+    var date = a.data('date')
+    var time = a.data('time')
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('#event-title').append(title)
+    modal.find('#event-location').append(location)
+    modal.find('#event-date').append(date)
+    modal.find('#event-time').append(time)
+  });
 
